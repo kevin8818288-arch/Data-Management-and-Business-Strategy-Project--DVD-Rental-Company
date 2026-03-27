28e8d90f59cc10dd23604a6c47542c4fdc25d014
Kevin Yu <kevin8818288@gmail.com>
2026-03-27 16:43:32 -0500

    Update 02_data_mart.sql

--- sql/02_data_mart.sql
+++ sql/02_data_mart.sql
@@ -1,1 +1,9 @@
+The company lacked a structured data model to understand:
 
+Which films and categories drive the most revenue?
+Which films have high demand but weak monetization / inefficient inventory?
+What pricing and inventory actions could improve revenue?
+The goal was to develop a data-driven strategy to improve revenue and operational efficiency.
+
+- Revenue Mart
+- to see 
