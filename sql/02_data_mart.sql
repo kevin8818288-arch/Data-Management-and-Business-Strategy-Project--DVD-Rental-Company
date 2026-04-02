35d75f1d025b1bb43716067e3a3c89667fdeed5e
Kevin Yu <kevin8818288@gmail.com>
2026-04-02 13:42:55 -0500

    Update 02_data_mart.sql

--- sql/02_data_mart.sql
+++ sql/02_data_mart.sql
@@ -1,14 +1,7 @@
--- The company lacked a structured data model to understand:
-
--- Which films and categories drive the most revenue?
--- Which films have high demand but weak monetization / inefficient inventory?
--- What pricing and inventory actions could improve revenue?
--- The goal was to develop a data-driven strategy to improve revenue and operational efficiency.
-
-- Revenue Mart
-- to see 
-  - 1. which film/ categories is revenue drivers
-  - 2. which titles should be focused on
+-- Revenue Mart
+-- Purpose:
+  -- 1. Identify top revenue-generating films and categories
+  -- 2. Evaluate revenue concentration and key business drivers
 CREATE VIEW revenue_mart AS
 SELECT
     film_id,
@@ -20,11 +13,13 @@ SELECT
     DENSE_RANK() OVER (ORDER BY total_revenue DESC) AS revenue_rank
 FROM movie_performance_dw;
 
---------------------------------------------------------
-- Demand Monetization Mart
-- to see
-  - 1. Which films have high demand but low return
-  - 2. Which films may have price inefficiency
+-- ------------------------------------------------------
+-- Demand Monetization Mart
+-- Purpose:
+  -- 1. Compare demand (rental volume) with revenue contribution
+  -- 2. Identify films with high demand but low monetization efficiency
+  -- 3. Detect potential pricing inefficiencies
+
 CREATE VIEW demand_monetization_mart AS
 SELECT
     film_id,
@@ -37,9 +32,9 @@ SELECT
     DENSE_RANK() OVER (ORDER BY rental_count DESC) AS demand_rank,
     DENSE_RANK() OVER (ORDER BY total_revenue DESC) AS revenue_rank
 FROM movie_performance_dw;
----------------------------------------------------------- 
-- Inventory Efficiency Mart
-- to see
+-- -------------------------------------------------------- 
+-- Inventory Efficiency Mart
+-- Purpose:
   - 1. Which films have bad inventory allocation efficiency
   - 2. Which films do not have sufficient inventory to meet the demand
   - 3. Which films are oversupply
@@ -55,9 +50,11 @@ SELECT
     revenue_per_inventory
 FROM movie_performance_dw;
 -------------------------------------------------------------
-- ROI Efficiency Mart 
-  - to see
-    - Which film have the highest investment efficiency
+-- ROI Efficiency Mart 
+  -- Purpose: 
+    -- 1. Estimate capital efficiency using replacement cost as a proxy
+    -- 2. Identify films with high or low return relative to inventory investment
+
 CREATE VIEW roi_efficiency_mart AS
 SELECT
     film_id,
